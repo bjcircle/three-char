2209c0618554fd2e072d93639a06dcc7632ce248
restService <basz.worapol@gmail.com>
2015-05-04 16:20:59 +0700

    y

---
+++ src/main/java/com/rest/rest/TestModel.java
@@ -0,0 +1,28 @@
+package com.rest.rest;
+
+public class TestModel {
+	private String A1;
+	private int A2;
+	private String A3;
+	
+	public String getA1() {
+		return A1;
+	}
+	public void setA1(String a1) {
+		A1 = a1;
+	}
+	public int getA2() {
+		return A2;
+	}
+	public void setA2(int a2) {
+		A2 = a2;
+	}
+	public String getA3() {
+		return A3;
+	}
+	public void setA3(String a3) {
+		A3 = a3;
+	}
+	
+	
+}
---
+++ src/main/java/com/rest/servlet/callRestServlet.java
@@ -0,0 +1,30 @@
+package com.rest.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/callRestServlet.do")
+public class callRestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public callRestServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	private void processRequest(HttpServletRequest request,
+			HttpServletResponse response) {		
+		
+	}
+}
